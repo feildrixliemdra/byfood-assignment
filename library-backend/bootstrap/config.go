eb4063cae587b685f4cfd0ffccda298a4af662cc
feildrixliemdra <feildrixliemdra@gmail.com>
2025-08-11 22:59:03 +0700

    feat(config): enhance configuration handling with required key checks and improved error logging

--- library-backend/bootstrap/config.go
+++ library-backend/bootstrap/config.go
@@ -1,8 +1,13 @@
 package bootstrap
 
 import (
+	"fmt"
 	"library-backend/internal/config"
+	"log"
 	"log/slog"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -14,13 +19,71 @@ func NewConfig() *config.Config {
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		slog.Warn("Error reading config file: %v", err)
+		slog.Warn("Error reading config file", "error", err)
 	}
 
-	var config config.Config
-	if err := viper.Unmarshal(&config); err != nil {
-		slog.Warn("Unable to decode config: %v", err)
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		slog.Warn("error reading config file", "error", err)
 	}
 
-	return &config
+	cfg := config.Config{
+		AppPort:       viper.GetString("APP_PORT"),
+		AppName:       viper.GetString("APP_NAME"),
+		DBURL:         getRequiredString("DB_URL"),
+		DBMaxIdleConn: getEnvAsInt("DB_MAX_IDLE_CONN", 10),
+		DBMaxOpenConn: getEnvAsInt("DB_MAX_OPEN_CONN", 10),
+	}
+
+	return &cfg
+}
+
+func getRequiredString(key string) string {
+	if viper.IsSet(key) {
+		return viper.GetString(key)
+	}
+
+	log.Fatalln(fmt.Errorf("KEY %s IS MISSING", key))
+	return ""
+}
+
+func getRequiredInt(key string) int {
+	if viper.IsSet(key) {
+		return viper.GetInt(key)
+	}
+
+	panic(fmt.Errorf("KEY %s IS MISSING", key))
+}
+
+func getRequiredTime(key string) time.Duration {
+	if viper.IsSet(key) {
+		return time.Duration(viper.GetInt(key)) * time.Second
+	}
+
+	panic(fmt.Errorf("KEY %s IS MISSING", key))
+}
+
+func getRequiredBool(key string) bool {
+	if viper.IsSet(key) {
+		return viper.GetBool(key)
+	}
+
+	panic(fmt.Errorf("KEY %s IS MISSING", key))
+}
+
+func GetEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+func getEnvAsInt(name string, defaultVal int) int {
+	valStr := GetEnv(name, "")
+	if value, err := strconv.Atoi(valStr); err == nil {
+		return value
+	}
+
+	return defaultVal
 }
